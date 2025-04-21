f87f011ea3d4c30686f883bbbeecd8fba3231644
Harryrana98 <ranaharendra77@gmail.com>
2025-04-21 17:19:48 +0530

    min and max

--- script.js
+++ script.js
@@ -1,60 +1,51 @@
-const form=document.querySelector("form")
-const input1=document.querySelector("#input1")
-const input2=document.querySelector("#input2")
-const result=document.querySelector(".result")
+const form = document.querySelector("form");
+const input1 = document.querySelector("#input1");
+const input2 = document.querySelector("#input2");
+const result = document.querySelector(".result");
 
-let arr=[]
+// let arr = [];
 
-// form.addEventListener("submit",(e)=>{
-//     e.preventDefault()
-//     let fInp=Number(input1.value)
-//     let lInp=Number(input2.value)
-//     if(fInp>lInp){
-//         console.log("hello");
-//         result.innerHTML=fInp-lInp
-        
-//             // RandomNumber()
-//     }else {
-//         alert("Please Enter the Grater Value")
-//         input1.value=""
-//         input2.value=""
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   let fInp = Number(input1.value);
+//   let lInp = Number(input2.value);
+//   if (fInp < lInp) {
+//     for (let i = fInp; i <= lInp; i++) {
+//       arr.push(i);
 //     }
+//     RandomNumber();
+//   } else {
+//     alert("Please Enter the Grater Value");
+//     input1.value = "";
+//     input2.value = "";
+//   }
+// });
 
-// })
+// function RandomNumber() {
+//   const Random = Math.floor(Math.random() * arr.length);
+// //   console.log("hello");
 
-// function RandomNumber(){
-//     const Random=Math.floor(Math.random())
-//     console.log(Random);
-    
+//   console.log(Random);
+
+//   let RandomIndex = arr[Random];
+
+//   result.innerHTML = RandomIndex;
 // }
 
+// console.log(arr.length);
 
-form.addEventListener("submit",function(e){
-    e.preventDefault()
-    let fInp=Number(input1.value)
-    let lInp=Number(input2.value)
-    if(fInp<lInp){
-        for(let i=fInp;i<=lInp;i++){
-           arr.push(i)
-           
-        }
-        RandomNumber()
-    }else {
-        alert("Please Enter the Grater Value")
-        input1.value=""
-        input2.value=""
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let max = Number(input1.value);
+  let min = Number(input2.value);
+  if (max < min) {
+    function getRandomNumber(min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
     }
-    
-})
-
-function RandomNumber(){
-
-    const Random=Math.floor(Math.random()*arr.length)
-    let RandomIndex=arr[Random]
-   
-    console.log(RandomIndex);
-    result.innerHTML=RandomIndex
-    
-}
-
-console.log(arr);
+    let RandomIndex = getRandomNumber(min, max);
+    result.innerHTML = RandomIndex;
+  } else {
+    alert("Please Enter The Max in 1st Value ");
+  }
+});
